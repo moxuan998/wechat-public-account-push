0686d419fddf21f8bb6285650cf0290fca034b57
moxuan998 <101006830+moxuan998@users.noreply.github.com>
2024-02-01 01:01:53 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,28 +8,28 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx4a52080bf2e9e8ae',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: '5d92d3d83e8e18fcd17481c2ca99dbe2',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '三明',
+  CITY: '屏山乡兴屏街',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '小笨猪',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oE1Jz6QHxFLa-rQZo31jhAYBkZuM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: '79dXti9uM2UOl8Oufa3lcSZ-dZHmUdiX3Rl1dxSqv0c',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '03-12',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '小笨蛋', year: '2003', date: '03-12',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
@@ -39,7 +39,7 @@ const USER_CONFIG = {
           type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
         },
       ],
-      // 我们在一起已经有xxxx天了的配置
+      // 我们认识已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
         { keyword: 'love_day', date: '2022-09-08' },
